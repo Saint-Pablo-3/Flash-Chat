62c4fb2a5e96a481fd0380442bc3f65bad5cbf89
Pablo <pawellreshet@icloud.com>
2023-02-02 20:03:15 +0300

    first commit registerView - constrains

---
+++ Flash Chat/Controllers/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  Flash Chat
+//
+//  Created by админ on 02.02.2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    @IBAction func loginPressed(_ sender: UIButton) {
+    }
+  
+
+}
---
+++ Flash Chat/Controllers/RegisterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterViewController.swift
+//  Flash Chat
+//
+//  Created by админ on 02.02.2023.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    @IBAction func registerPressed(_ sender: UIButton) {
+    }
+    
+
+
+}
